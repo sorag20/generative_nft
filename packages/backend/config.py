05bc28d3143a62daed162a5a3d781fde84545066
sorag <gkthn20@gmail.com>
2023-09-01 08:06:10 +0900

    metadata作成まで

---
+++ packages/backend/config.py
@@ -0,0 +1,113 @@
+
+METADATA = {
+    "name": "test",
+    "description": "generative NFT",
+    "external_url": "https://test/",
+}
+
+CONFIG = [
+    {
+        "id": 1,
+        "name": "1",
+        "directory": "1",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+     {
+        "id": 2,
+        "name": "2",
+        "directory": "2",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+       {
+        "id": 3,
+        "name": "3",
+        "directory": "3",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+     {
+        "id": 4,
+        "name": "4",
+        "directory": "4",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+     {
+        "id": 5,
+        "name": "5",
+        "directory": "5",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+    {
+        "id": 6,
+        "name": "6",
+        "directory": "6",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+    {
+        "id": 7,
+        "name": "7",
+        "directory": "7",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+    {
+        "id": 8,
+        "name": "8",
+        "directory": "8",
+        "required": True,
+        "rarity_weights": [
+            1,  
+            1,  
+            1,  
+            1,  
+            1, 
+        ],
+    },
+]
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
-async function generate(){
-  const res= await fetch("localhost:8000/generate",{
-    method: 'POST'
-  })
-  
-
-}
+  const [count, setCount] = useState('');
+  const handleSubmit = async () => {
+    try {
+      const res = await fetch('http://localhost:8000/generate', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ count: count }),
+      })
+        .then(function (response) {
+          return response.text();
+        })
+        .then(function (data) {
+          alert(data); // this will be a string
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className="App">
-     <button onClick={() => generate()}>generate</button>
-    </div>
+    <>
+      <label
+        style={{
+          fontSize: '20px',
+          display: 'block',
+          transform: 'translate(200px,200px)',
+        }}
+      >
+        生成画像数
+      </label>
+      <input
+        type="text"
+        value={count}
+        onChange={(event) => setCount(event.target.value)}
+        style={{
+          width: 120,
+          height: 50,
+          transform: 'translate(200px,200px)',
+        }}
+      />
+      <button
+        onClick={handleSubmit}
+        type="submit"
+        style={{ width: 120, height: 55, transform: 'translate(200px,200px)' }}
+      >
+        Generate
+      </button>
+    </>
   );
 }
 
